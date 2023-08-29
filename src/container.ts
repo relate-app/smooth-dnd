f131e1e98559afc9679e258c5fac1c8e48a6140e
Tim Brandin <tim.brandin@gmail.com>
2023-08-29 17:18:16 +0200

    Testing reset after onDrop handler

--- src/container.ts
+++ src/container.ts
@@ -157,12 +157,11 @@ function handleDrop({ element, draggables, layout, getOptions }: ContainerProps)
   const draggablesReset = resetDraggables({ element, draggables, layout, getOptions });
   const dropHandler = (smoothDnD.dropHandler || domDropHandler)({ element, draggables, layout, getOptions });
   return function (draggableInfo: DraggableInfo, { addedIndex, removedIndex }: DragResult, forDispose: boolean = false) {
-    draggablesReset();
     // if drop zone is valid => complete drag else do nothing everything will be reverted by draggablesReset()
     if (!draggableInfo.cancelDrop) {
       if (draggableInfo.targetElement || getOptions().removeOnDropOut || forDispose) {
         let actualAddIndex =
-          addedIndex !== null ? (removedIndex !== null && removedIndex < addedIndex ? addedIndex - 1 : addedIndex) : null;
+        addedIndex !== null ? (removedIndex !== null && removedIndex < addedIndex ? addedIndex - 1 : addedIndex) : null;
         const dropHandlerParams = {
           removedIndex,
           addedIndex: actualAddIndex,
@@ -172,6 +171,7 @@ function handleDrop({ element, draggables, layout, getOptions }: ContainerProps)
         dropHandler(dropHandlerParams, getOptions().onDrop);
       }
     }
+    draggablesReset();
   };
 }
 
